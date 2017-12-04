9afdc1f4a3259debe1672a5c10c8062667c76dcd
joshwarren <joshua.warren@physics.ox.ac.uk>
2017-12-04 22:54:56 +0000

    EoDC: Chapter 5 nearly finished
    Nearly done with all plots sorted.
    
    Just waiting on whole_image[_muse].py
    
     On branch chapter5
     Changes to be committed:
    	new file:   chapter5/BPT.png
    	modified:   chapter5/NII_NI_ratio.png
    	new file:   chapter5/SAURON.png
    	new file:   chapter5/WHaN2.png
    	new file:   chapter5/WHbN1.png
    	modified:   chapter5/chapter5.aux
    	modified:   chapter5/chapter5.tex
    	new file:   chapter5/muse/Halpha_profile.png
    	new file:   chapter5/muse/Hb.png
    	modified:   chapter5/muse/kin.png
    	new file:   chapter5/ngc1316_inflow.png
    	new file:   chapter5/nuclear_MEx.png
    	modified:   chapter5/produce_Hb.py
    	modified:   chapter5/produce_Hb_muse.py
    	modified:   chapter5/produce_kine_plot.py
    	new file:   chapter5/stellar_ratio.png
    	modified:   chapter5/vimos/Hb.png
    	new file:   chapter5/vimos/Hbeta_profile.png
    	modified:   chapter5/vimos/kin.png
    	new file:   chapter5/vimos/ngc0612_Hb.png
    	modified:   refs.bib
    	modified:   thesis.aux
    	modified:   thesis.bbl
    	modified:   thesis.blg
    	modified:   thesis.lof
    	modified:   thesis.log
    	modified:   thesis.out
    	modified:   thesis.toc

--- chapter5/produce_Hb_muse.py
+++ chapter5/produce_Hb_muse.py
@@ -7,7 +7,7 @@ import cPickle as pickle
 import matplotlib.pyplot as plt 
 import numpy as np 
 from plot_velfield_nointerp import plot_velfield_nointerp
-from plot_results import add_
+from plot_results_muse import add_
 from errors2_muse import get_dataCubeDirectory
 from prefig import Prefig
 from astropy.io import fits 
@@ -18,7 +18,7 @@ from plot_results import set_lims
 def plot(galaxies):
 	opt = 'pop'
 	overplot={'CO':'c', 'radio':'r'}
-	Prefig(size=np.array((4, 1))*10)
+	Prefig(size=np.array((4, 1))*8)
 	fig, axs = plt.subplots(1, 4)#, sharex=True, sharey=True)
 	out_dir = '%s/Documents/thesis/chapter5/muse' % (cc.home_dir)
 
@@ -34,7 +34,7 @@ def plot(galaxies):
 	# 		self.SNRatio = np.array([0,1,1,2])
 	# 		self.unbinned_flux = np.zeros((40,40))
 	# 		self.number_of_bins = 4
-	# 		self.components = {'stellar':comp(),'Hbeta':comp()}
+	# 		self.components = {'stellar':comp(),'[OIII]5007d':comp()}
 	# 		self.e_line = {f:v for f,v in self.components.iteritems()
 	# 			if f!='stellar'}
 	# 		self.flux = np.array([0,1,1,2])
@@ -76,37 +76,48 @@ def plot(galaxies):
 		pickleFile.close()
 
 		f = fits.open(get_dataCubeDirectory(galaxy))
-		header = f[0].header
+		header = f[1].header
 		f.close()
 
-		axs.flatten()[i] = plot_velfield_nointerp(D.x, D.y, D.bin_num, 
-			D.xBar, D.yBar, D.components['Hbeta'].flux, header,  
+		axs[i] = plot_velfield_nointerp(D.x, D.y, D.bin_num, 
+			D.xBar, D.yBar, D.components['[OIII]5007d'].flux, header,  
 			# vmin=vmin[attr==plots[0]], vmax=vmax[attr==plots[0]], 
-			cmap=sauron, flux_unbinned=D.unbinned_flux, 
-			signal_noise=D.e_line['Hbeta'].amp_noise, signal_noise_target=4,
-			galaxy=galaxy, ax=axs.flatten()[i])
+			cmap=sauron, flux_unbinned=D.unbinned_flux, galaxy_labelcolor='w',
+			signal_noise=D.e_line['[OIII]5007d'].amp_noise, signal_noise_target=4,
+			galaxy=galaxy, ax=axs[i])
 		if overplot:
 			for o, color in overplot.iteritems():
-				add_(o, color, axs.flatten()[i], galaxy, nolegend=True)
+				add_(o, color, axs[i], galaxy, nolegend=True)
 
-	for a in axs.flatten():
+	for a in axs:
+		if hasattr(a, 'ax_dis'):
+			a.ax_dis.tick_params(top=True, bottom=True, left=True, 
+				right=True, direction='in', which='major', length=20,
+				width=3, labelsize='large')
+			a.ax_dis.tick_params(top=True, bottom=True, left=True, 
+				right=True, direction='in', which='minor', length=10,
+				width=3)
+			a.ax_dis.xaxis.label.set_size(22)
+			a.ax_dis.yaxis.label.set_size(22)
+
+	for a in axs:
 		if hasattr(a, 'ax_dis'):
 			a.ax_dis.tick_params(top=True, bottom=True, left=True, right=True, 
 				direction='in', which='both')
 
-	for a in axs.flatten()[[1,2,3,5,6,7,10]]:
+	for a in axs[1:]:
 		if hasattr(a, 'ax_dis'): 
 			a.ax_dis.set_yticklabels([])
 			a.ax_dis.set_ylabel('')
 
-	for a in axs.flatten()[[0,1,2,3,5,6]]:
-		if hasattr(a, 'ax_dis'): 
-			a.ax_dis.set_xticklabels([])
-			a.ax_dis.set_xlabel('')
+	# Add colorbar
+	ax_loc = axs[3].get_position()
+	cax = fig.add_axes([ax_loc.x1+0.03, ax_loc.y0, 0.02, ax_loc.height])
+	cbar = plt.colorbar(axs[0].cs, cax=cax)
+	cbar.ax.set_yticklabels([])
 
 
-
-	fig.savefig('%s/Hb.png' % (out_dir), bbox_inches='tight')
+	fig.savefig('%s/Hb.png' % (out_dir), bbox_inches='tight', dpi=120)
 
 
 
