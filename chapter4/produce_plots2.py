56a6318b6f2bd76c6c6dbab3b46b6d306e9d03a6
joshwarren <joshua.warren@physics.ox.ac.uk>
2018-02-09 13:44:23 +0000

    EoDC:
    produce_plots2.py: Futher work on absorption line plots and started on
    showing ppxf fits.
    
     On branch master
     Changes to be committed:
    	modified:   chapter4/produce_plots2.py

--- chapter4/produce_plots2.py
+++ chapter4/produce_plots2.py
@@ -8,7 +8,7 @@ if 'home' not in cc.device:
 import matplotlib.pyplot as plt 
 from prefig import Prefig
 from Bin2 import Data
-import corner
+# import corner
 from tools import myerrorbar
 
 def populations_corner(galaxy, instrument='vimos'):
@@ -49,7 +49,7 @@ def populations_corner(galaxy, instrument='vimos'):
 	# 			gt_fwhm = hist >= np.max(hist)/2
 	# 			unc_plot[j] = np.max(x[gt_fwhm]) - np.min(x[gt_fwhm])
 
-def add_grid(ax, xline, yline, alpha=0.5):
+def add_grid(ax, xline, yline, alpha=0.5, show_alpha=True, show_labels=True):
 	age,Z_H,alpha_fe,HdA,HdF,CN1,CN2,Ca4227,G4300,HgA,HgF,Fe4383,Ca4455,Fe4531,\
 		C24668,Hb,Fe5015,Mg1,Mg2,Mgb,Fe5270,Fe5335,Fe5406,Fe5709,Fe5782,NaD,TiO1,\
 		TiO2=np.genfromtxt("%s/models/TMJ_SSPs/tmj.dat" % (cc.home_dir), 
@@ -72,16 +72,35 @@ def add_grid(ax, xline, yline, alpha=0.5):
 	for j in range(x_grid.shape[1]):
 		ax.plot(x_grid[:,j], y_grid[:,j], 'k--')
 
+	ax.arrow(x_grid[0,0], y_grid[0,0], (x_grid[1,0]-x_grid[0,0])/3., 
+		(y_grid[1,0]-y_grid[0,0])/3., ls='-', head_width=0.2, head_length=0.2,
+		fc="k", ec="w", zorder=-1)
+	ax.arrow(x_grid[0,0], y_grid[0,0], (x_grid[0,1]-x_grid[0,0])/3., 
+		(y_grid[0,1]-y_grid[0,0])/3., ls='-', head_width=0.2, head_length=0.2,
+		fc="k", ec="w", zorder=-1)
 
+	if show_labels:
+		ax.text(x_grid[0,0] + (x_grid[1,0]-x_grid[0,0])/3., 
+			y_grid[0,0] + (y_grid[1,0]-y_grid[0,0])/3., 'Age', va='top', 
+			ha='center')
+		ax.text(x_grid[0,0] + (x_grid[0,1]-x_grid[0,0])/3. - 0.8, 
+			y_grid[0,0] + (y_grid[0,1]-y_grid[0,0])/3. + 0.1, 'Metalicity', 
+			va='bottom', ha='left')
+	if show_alpha:
+		ax.text(0.98,0.98, 'Alpha enhancement = %s'%(alpha), transform=ax.transAxes, 
+			va='top', ha='right')
 
-
-
-
-
-
-
+# plot each individual galaxy
 def absorption_plots(galaxy, instrument='vimos'):
+	print galaxy, instrument
 	D = Data(galaxy, instrument=instrument, opt='pop')
+	# if True:
+	#  	import cPickle as pickle
+	#  	pickle_file = '%s/Data/%s/analysis/%s/pop/pickled' % (cc.base_dir, 
+	#  		instrument, galaxy)
+	# 	pickleFile = open("%s/dataObj.pkl" % (pickle_file), 'rb')
+	# 	D = pickle.load(pickleFile)
+	# 	pickleFile.close()
 
 	fig, ax = plt.subplots()
 	# add_grid(ax, 'Hb', 'MgFe')
@@ -89,22 +108,135 @@ def absorption_plots(galaxy, instrument='vimos'):
 	xlim = ax.get_xlim()
 	ylim = ax.get_ylim()
 
+	# Subtract all fitted emission lines
+	Hb, e_Hb = D.absorption_line('H_beta', uncert=True, nomask=True)
+	Mg, e_Mg = D.absorption_line('Mgb', uncert=True, nomask=True)
+	# Hb, e_Hb = D.absorption_line('H_beta', uncert=True, nomask=False)
+	# Mg, e_Mg = D.absorption_line('Mgb', uncert=True, nomask=False)
+	# r = np.sqrt((D.xBar - D.center[0])**2 + (D.yBar - D.center[1])**2)
+	r = np.sqrt((D.xBar)**2 + (D.yBar)**2)
 
-	Hb, e_Hb = D.absorption_line('H_beta', uncert=True)
-	Mg, e_Mg = D.absorption_line('Mgb', uncert=True)
-	r = np.sqrt((D.xBar - D.center[0])**2 + (D.yBar - D.center[1])**2)
+	# Hb = np.append(Hb, [np.nan, np.nan])
+	# Mg = np.append(Mg, [np.nan, np.nan])
+	# e_Hb = np.append(e_Hb, [0, 0])
+	# e_Mg = np.append(e_Mg, [0, 0])
+	if instrument == 'vimos':
+		vmax = 20*np.sqrt(2)
+	elif instrument == 'muse':
+		vmax = 75*np.sqrt(2)
 
-	myerrorbar(ax, Hb, Mg, xerr=e_Hb, yerr=e_Mg, color=r, marker='.')
+	m = ((e_Hb < 1) * (e_Mg < 1))
+	m2 = np.zeros(len(Hb)).astype(bool)
+	m2[::10] = True
+	# m2[-2:] = True
+	m *= m2
+
+	# print r[m].min(), r[m].max()
+	# if not any(m):
+	# 	m = np.ones(len(e_Hb)).astype(bool)
+
+	myerrorbar(ax, Hb[m], Mg[m], xerr=e_Hb[m], yerr=e_Mg[m], color=r[m], marker='.',
+		colorbar=True, cmap='viridis_r', vmin=0, vmax=vmax)
 
 	ax.set_xlim(xlim)
 	ax.set_ylim(ylim)
 
+	ax.set_xlabel(r'H$\,\beta$')
+	ax.set_ylabel(r'Mg$\,$b')
+
+	fig.savefig('%s/absorption_plots_%s.png' % (instrument, galaxy))
+
+	plt.close('all')
+
+
+def single_absorption_plot(instrument='vimos'):
+	if instrument == 'vimos':
+		galaxies = ['eso443-g024', 'ic1459', 'ic1531', 'ic4296', 'ngc1399', 
+			'ngc3100', 'ngc3557', 'ngc7075'
+		]
+		str_galaxies = ['ESO 443-G24', 'IC 1459', 'IC 1531', 'IC 4296', 'NGC 1399',
+			'NGC 3100', 'NGC 3557', 'NGC 7075'
+			]
+		Prefig(size=np.array((4, 2))*7)
+		fig, ax = plt.subplots(2, 4, sharex=True, sharey=True)
+		vmax = 20*np.sqrt(2)
+		res = 0.67
+
+	elif instrument == 'muse':
+		galaxies = ['ic1459', 'ic4296', 'ngc1316', 'ngc1399']
+		str_galaxies = ['IC 1459', 'IC 4296', 'NGC 1316', 'NGC 1399']
+		Prefig(size=np.array((4, 2))*7)
+		fig, ax = plt.subplots(1, 4, sharex=True, sharey=True)
+		vmax = 75*np.sqrt(2)
+		res = 0.2
+
+	for i, a in enumerate(ax.flatten()):
+		if i == 0:
+			add_grid(a, 'Hb', 'Mgb', show_alpha=False)
+		else:
+			add_grid(a, 'Hb', 'Mgb', show_alpha=False, show_labels=False)
+		
+	xlim = ax[0,0].get_xlim()
+	ylim = ax[0,0].get_ylim()
+
+
+
+	for i, galaxy in enumerate(galaxies):
+		D = Data(galaxy, instrument=instrument, opt='pop')
+
+		Hb, e_Hb = D.absorption_line('H_beta', uncert=True, nomask=True)
+		Mg, e_Mg = D.absorption_line('Mgb', uncert=True, nomask=True)
+		r = np.sqrt((D.xBar)**2 + (D.yBar)**2) * res
+
+		m = ((np.abs(e_Hb) < 1) * (np.abs(e_Mg) < 1))
+		# m2 = np.zeros(len(Hb)).astype(bool)
+		# m2[::10] = True
+		# m *= m2
+		myerrorbar(ax.flatten()[i], Hb[m], Mg[m], xerr=e_Hb[m], yerr=e_Mg[m], 
+			color=r[m], marker='.', cmap='viridis_r', vmin=0, vmax=vmax)
+		ax.flatten()[i].text(5,6.5, str_galaxies[i])
+
+
+	fig.text(0.1, 0.5, r'H$\,\beta$', va='center', ha='right',
+			rotation='vertical', size='xx-large')
+	fig.text(0.52, 0.09, r'Mg$\,$b', va='top', ha='center', size='xx-large')
+
+	ax[0,0].set_xlim(xlim)
+	ax[0,0].set_ylim(ylim)
+
+	ax_loc = ax[0,3].get_position()
+	cax = fig.add_axes([ax_loc.x1+0.03, ax_loc.y0, 0.02, ax_loc.height])
+	cbar = plt.colorbar(ax[0,0].sc, cax=cax)
+	t = cbar.ax.text(2.0,0.5, 'Distance (arcsec)', rotation=270, 
+		verticalalignment='center')
+	# cbar.ax.set_yticklabels([])
+
+	fig.subplots_adjust(wspace=0,hspace=0)
+	fig.savefig('%s/absorption.png' % (instrument), bbox_inches='tight', dpi=200)
+
+
+
+
+
+
+def plot_bin(ax, galaxy, bin_num, instrument='vimos', opt='kin'):
+	D = Data(galaxy, instrument=instrument, opt=opt)
+
+	b = D.bin[bin_num]
+
+	lam = b.lam
+	spectrum = b.spectrum
+	bestfit = b.bestfit
+
+	ax.plot(lam, spectrum, 'k', label='Data')
+	ax.plot(lam, bestfit, 'r', label='Bestfit')
+
+	ax2 = ax.twinx()
+
+	ax2.plot(lam, spectrum - bestfit, '.', color='LimeGreen', 
+		mec='LimeGreen', ms=4, label='Residuals')
+
 	plt.show()
-
-	
-
-
-
 
 
 
@@ -117,5 +249,19 @@ def absorption_plots(galaxy, instrument='vimos'):
 
 
 if __name__=='__main__':
-	# populations_corner('eso443-g024', instrument='vimos')
-	absorption_plots('ic1459', instrument='vimos')
+	if 'home' in cc.device:
+		# for galaxy in ['eso443-g024', 'ic1459', 'ic1531', 'ic4296', 
+		# 	'ngc1399', 'ngc3100', 'ngc3557', 'ngc7075']:
+		# 	# populations_corner('eso443-g024', instrument='vimos')
+		# 	absorption_plots(galaxy, instrument='vimos')
+		# single_absorption_plot(instrument='vimos')
+
+
+
+		fig,ax = plt.subplots()
+		plot_bin(ax, 'ic1459', 100, opt='pop')
+
+	elif cc.device == 'uni':
+		for galaxy in ['ic1459', 'ic4296', 'ngc1316', 'ngc1399']:
+			# populations_corner('eso443-g024', instrument='muse')
+			absorption_plots(galaxy, instrument='muse')
